2aafbd18f6e63994ea3f3a40d624654181e2b5c8
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2024-02-27 13:21:31 -0300

    feat: trata erro 500 do token validator

--- src/presentation/controllers/send-message.ts
+++ src/presentation/controllers/send-message.ts
@@ -4,6 +4,7 @@ import { badRequest } from '../helpers/http-helper';
 import { Controller } from '../protocols/controller';
 import { TokenValidator } from '../protocols/token-validator';
 import { InvalidToken } from '../errors/invalid-token-error';
+import { ServerError } from '../errors/server-error';
 
 export class SendMessageController implements Controller {
   private readonly tokenValidator: TokenValidator;
@@ -12,13 +13,22 @@ export class SendMessageController implements Controller {
     this.tokenValidator = tokenValidator;
   }
   handle(httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.message) {
-      return badRequest(new MissingParamError('message'));
-    }
+    try {
+      if (!httpRequest.body.message) {
+        return badRequest(new MissingParamError('message'));
+      }
 
-    const isValidToken = this.tokenValidator.isValid(httpRequest.body.message);
-    if (!isValidToken) {
-      return badRequest(new InvalidToken('token'));
+      const isValidToken = this.tokenValidator.isValid(
+        httpRequest.body.message,
+      );
+      if (!isValidToken) {
+        return badRequest(new InvalidToken('token'));
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError(),
+      };
     }
   }
 }
