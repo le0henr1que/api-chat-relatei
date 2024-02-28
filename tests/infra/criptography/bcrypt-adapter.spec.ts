2d88734e4b38b37930f7b1c716b47ec50bacea83
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2024-02-28 16:28:08 -0300

    chore: add jest config mongodb

--- tests/infra/criptography/bcrypt-adapter.spec.ts
+++ tests/infra/criptography/bcrypt-adapter.spec.ts
@@ -7,20 +7,34 @@ jest.mock('bcrypt', () => ({
   },
 }));
 
+const salt = 12;
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter(salt);
+};
+
 describe('BcryptAdapter', () => {
   test('Should call bcrypt with correct values', async () => {
-    // const { sut, salt } = makeSut();
-    const salt = 12;
-    const sut = new BcryptAdapter(salt);
+    const sut = makeSut();
+
     const hashSpy = jest.spyOn(bcrypt, 'hash');
     await sut.encrypt('any_value');
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt);
   });
   test('Should creturn a hash on success', async () => {
-    // const { sut, salt } = makeSut();
-    const salt = 12;
-    const sut = new BcryptAdapter(salt);
+    const sut = makeSut();
+
     const hash = await sut.encrypt('any_value');
     expect(hash).toBe('hash');
   });
+  // test('Should throw if bcrypt throws', async () => {
+  //   const sut = makeSut();
+  //   jest
+  //     .spyOn(bcrypt, 'hash')
+  //     .mockReturnValueOnce(
+  //       new Promise<string>((_, reject) => reject(new Error())),
+  //     );
+
+  //   const promise = sut.encrypt('any_value');
+  //   await expect(promise).rejects.toThrow();
+  // });
 });
