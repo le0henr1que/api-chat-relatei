521ab3aefcbf1050cd2c1b0ef18b5dbe01971ac3
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2024-02-28 11:50:01 -0300

    teste: hash sucesso do bcrypt e mock

--- tests/infra/criptography/bcrypt-adapter.spec.ts
+++ tests/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt';
 import { BcryptAdapter } from '@/infra/criptography/bcrypt-adapter';
 
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return new Promise((resolve) => resolve('hash'));
+  },
+}));
+
 describe('BcryptAdapter', () => {
   test('Should call bcrypt with correct values', async () => {
     // const { sut, salt } = makeSut();
@@ -9,5 +15,12 @@ describe('BcryptAdapter', () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash');
     await sut.encrypt('any_value');
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt);
+  });
+  test('Should creturn a hash on success', async () => {
+    // const { sut, salt } = makeSut();
+    const salt = 12;
+    const sut = new BcryptAdapter(salt);
+    const hash = await sut.encrypt('any_value');
+    expect(hash).toBe('hash');
   });
 });
