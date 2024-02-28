cef9e9739d9308a21299c523f422c5f24895d3c8
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2024-02-28 10:00:23 -0300

    refactor: movendo sut para um make

--- tests/data/use-cases/send-message.spec.ts
+++ tests/data/use-cases/send-message.spec.ts
@@ -1,14 +1,24 @@
 import { DbSendMessage } from '@/data/use-cases/db-send-message';
 
+interface sutTypes {
+  sut: DbSendMessage;
+  encrypterStub: any;
+}
+
+const makeSut = (): sutTypes => {
+  class EncrypterStub {
+    async encrypt(value: string): Promise<string> {
+      return new Promise((resolve) => resolve('hashed_message'));
+    }
+  }
+  const encrypterStub = new EncrypterStub();
+  const sut = new DbSendMessage(encrypterStub);
+  return { sut, encrypterStub };
+};
+
 describe('DbSendMessage', () => {
   test('Should call Encrypter message', async () => {
-    class EncrypterStub {
-      async encrypt(value: string): Promise<string> {
-        return new Promise((resolve) => resolve('hashed_message'));
-      }
-    }
-    const encrypterStub = new EncrypterStub();
-    const sut = new DbSendMessage(encrypterStub);
+    const { sut, encrypterStub } = makeSut();
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt');
     const message = { message: 'valid_message' };
     await sut.send(message);
