9fc6734874c6eb0d0075224a7c716407de4052d5
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2024-02-28 10:18:42 -0300

    test: db send message lancamento de erro

--- tests/data/use-cases/send-message.spec.ts
+++ tests/data/use-cases/send-message.spec.ts
@@ -1,17 +1,23 @@
 import { DbSendMessage } from '@/data/use-cases/db-send-message';
+import { Encrypter } from '../protocols/encript';
+import { mock } from 'node:test';
 
 interface sutTypes {
   sut: DbSendMessage;
   encrypterStub: any;
 }
 
-const makeSut = (): sutTypes => {
-  class EncrypterStub {
-    async encrypt(value: string): Promise<string> {
+const makeEncrypter = (): any => {
+  class EncrypterStub implements Encrypter {
+    async encrypt(_value: string): Promise<string> {
       return new Promise((resolve) => resolve('hashed_message'));
     }
   }
-  const encrypterStub = new EncrypterStub();
+  return new EncrypterStub();
+};
+
+const makeSut = (): sutTypes => {
+  const encrypterStub = makeEncrypter();
   const sut = new DbSendMessage(encrypterStub);
   return { sut, encrypterStub };
 };
@@ -23,5 +29,14 @@ describe('DbSendMessage', () => {
     const message = { message: 'valid_message' };
     await sut.send(message);
     expect(encryptSpy).toHaveBeenCalledWith('valid_message');
+  });
+  test('Should throw if Encrypter throws', async () => {
+    const { sut, encrypterStub } = makeSut();
+    jest
+      .spyOn(encrypterStub, 'encrypt')
+      .mockImplementationOnce(() => Promise.reject(new Error()));
+    const message = { message: 'valid_message' };
+    const primise = sut.send(message);
+    expect(primise).rejects.toThrow();
   });
 });
